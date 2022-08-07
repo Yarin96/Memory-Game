b20e9f481c854a909cb2f597cb0bb4cc2460822d
Yarin Bar <yarin0008@gmail.com>
2022-08-07 11:24:11 +0300

    getters and setters on Player class

--- Ex02.UserInterface/Player.cs
+++ Ex02.UserInterface/Player.cs
@@ -4,21 +4,33 @@ namespace Ex02.UserInterface
 {
     internal class Player
     {
-        private ePlayerType m_Type;
+        private ePlayerType m_PlayerType;
         private string m_PlayerName;
         private int m_PlayerScore;
 
         public Player(ePlayerType i_Type, string i_PlayerName)
         {
-            m_Type = i_Type;
+            m_PlayerType = i_Type;
             m_PlayerName = i_PlayerName;
             m_PlayerScore = 0;
         }
 
-        public string Name
+        public ePlayerType PlayerType
+        {
+            get { return m_PlayerType; }
+            set { m_PlayerType = value; }
+        }
+
+        public string PlayerName
         {
             get { return m_PlayerName; }
             set { m_PlayerName = value; }
+        }
+
+        public int PlayerScore
+        {
+            get { return m_PlayerScore; }
+            set { m_PlayerScore = value; }
         }
     }
 }
