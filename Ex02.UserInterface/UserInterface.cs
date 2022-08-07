d5c1db24806004fc86c3d5a5040cf06dad59a029
Yarin Bar <yarin0008@gmail.com>
2022-08-07 12:42:00 +0300

    playerTurn function added

--- Ex02.UserInterface/UserInterface.cs
+++ Ex02.UserInterface/UserInterface.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Ex02.ConsoleUtils;
-using Ex02.Logic;
 using Ex02.Enums;
 
 namespace Ex02.UserInterface
@@ -33,32 +32,7 @@ namespace Ex02.UserInterface
             Player player2 = new Player(m_PlayerType, m_SecondPlayerName);
             m_Board = new Board(player1, player2, m_GameMode, int.Parse(m_BoardWidth), int.Parse(m_BoardHeight));
             m_Board.PrintBoard();
-            Console.WriteLine("Please choose cell:");
-            string choice = Console.ReadLine();
-            bool isLegal = m_Board.isCellLocationInputValid(choice);
-            bool isChosen = true;
-            while (!isLegal || isChosen)
-            {
-                if(!isLegal)
-                {
-                    Console.WriteLine("Not legal option, please choose again:");
-                    choice = Console.ReadLine();
-                    isLegal = m_Board.isCellLocationInputValid(choice);
-                    continue;
-                }
-                else
-                {
-                    isChosen = m_Board.isCellAlreadyChosen(choice);
-                    if(!isChosen)
-                    {
-                        continue;
-                    }
-
-                    Console.WriteLine("Card already been discovered, please choose again:");
-                    choice = Console.ReadLine();
-                    isLegal = m_Board.isCellLocationInputValid(choice);
-                }
-            }
+            playerTurn();
         }
 
         private void gameTitle()
@@ -166,6 +140,36 @@ namespace Ex02.UserInterface
             Console.WriteLine("\nWe're good to go! Press 'enter' to start the game ...");
             Console.ReadLine();
             Screen.Clear();
+        }
+
+        private void playerTurn()
+        {
+            Console.Write("Please choose a card (Format of [Capital Letter, Number]): ");
+            string choice = Console.ReadLine();
+            bool isLegal = m_Board.isCellLocationInputValid(choice);
+            bool isChosen = true;
+            while (!isLegal || isChosen)
+            {
+                if (!isLegal)
+                {
+                    Console.WriteLine("Not legal option, please choose again:");
+                    choice = Console.ReadLine();
+                    isLegal = m_Board.isCellLocationInputValid(choice);
+                    continue;
+                }
+                else
+                {
+                    isChosen = m_Board.isCellAlreadyChosen(choice);
+                    if (!isChosen)
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine("Card already been discovered, please choose again:");
+                    choice = Console.ReadLine();
+                    isLegal = m_Board.isCellLocationInputValid(choice);
+                }
+            }
         }
     }
 }
